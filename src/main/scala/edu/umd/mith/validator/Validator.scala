950b6d6fa48819e99d80a20bfa964a00956dd285
Travis Brown <travisrobertbrown@gmail.com>
2013-02-27 06:23:19 -0500

    Added some basic error handling.

--- src/main/scala/edu/umd/mith/validator/Validator.scala
+++ src/main/scala/edu/umd/mith/validator/Validator.scala
@@ -6,7 +6,7 @@ import org.scalatra.fileupload._
 import org.scalatra.scalate.ScalateSupport
 import org.scalatra.json._
 
-import java.io.{ File, InputStreamReader, StringReader }
+import java.io.{ File, InputStreamReader, Reader, StringReader }
 import javax.xml.XMLConstants
 import javax.xml.transform.stream.StreamResult
 import javax.xml.transform.stream.StreamSource
@@ -25,55 +25,61 @@ class Validator extends ValidatorStack {
     </html>
   }
 
+  def schemaSource = params.get("schema").map(new StreamSource(_)).getOrElse(
+    halt(404, "Not Found")
+  )
+
+  def documentParam = params.get("document").map(new StringReader(_))
+  def documentFileParam = fileParams.get("document").map(
+    document => new InputStreamReader(document.getInputStream)
+  )
+
+  def documentSource(reader: Option[Reader]) =
+    reader.map(new StreamSource(_)).getOrElse(
+      halt(400, "Bad Request")
+    )
+
+  def createValidator(schema: StreamSource) =
+    try SchemaFactory
+      .newInstance(XMLConstants.RELAXNG_NS_URI)
+      .newSchema(schema)
+      .newValidator
+    catch {
+      case _: SAXException => halt(400, "Bad Request")
+    }
+
   get("/validate") {
     contentType = formats("json")
 
-    val schemaUrl = params("schema")
-    val document = new StringReader(params("document"))
-
-    val rngValidator = SchemaFactory.newInstance(XMLConstants.RELAXNG_NS_URI)
-      .newSchema(new StreamSource(schemaUrl))
-      .newValidator
+    val document = documentSource(documentParam)
+    val validator = createValidator(schemaSource)
 
     val handler = new ValidationErrorHandler
-    rngValidator.setErrorHandler(handler)
+    validator.setErrorHandler(handler)
 
-    val errors = try {
-      rngValidator.validate(new StreamSource(document))
+    try {
+      validator.validate(document)
       handler.getErrors
     } catch {
       case _: SAXException => handler.getErrors
     }
-    
-    errors
   }
+
   post("/validate") {
     contentType = formats("json")
 
-    val schemaUrl = params("schema")
-
-    val source = new StreamSource(
-      params
-
-        .get("document")
-        .map(doc => new StringReader(doc))
-        .getOrElse(new InputStreamReader(fileParams("document").getInputStream))
-    )
-
-    val rngValidator = SchemaFactory.newInstance(XMLConstants.RELAXNG_NS_URI)
-      .newSchema(new StreamSource(schemaUrl))
-      .newValidator
+    val document = documentSource(documentParam orElse documentFileParam)
+    val validator = createValidator(schemaSource)
 
     val handler = new ValidationErrorHandler
-    rngValidator.setErrorHandler(handler)
+    validator.setErrorHandler(handler)
 
-    val errors = try {
-      rngValidator.validate(source)
+    try {
+      validator.validate(document)
       handler.getErrors
     } catch {
       case _: SAXException => handler.getErrors
     }
-    
-    errors
   }
 }
+
